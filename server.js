1f5907d6e8d8a64b89d6d0f75660670b57baf88d
Joseph Mutua <Mutuaj793@gmail.com>
2021-11-21 21:46:08 +0300

    Connect to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const port = process.env.port || 8000;
-const app = express()
+const mongoURI = process.env.MONGO_URI;
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
 
+//Connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => {
+    console.log(`DB CONNECTION ERROR: ${err.message}`);
+  });
 
 //import routes
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./routes/auth");
 
 //middleware
-app.use("/api",authRoutes)
+app.use("/api", authRoutes);
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
