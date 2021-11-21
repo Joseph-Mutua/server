bf8f0ea023ab1c897c0bf7260b10d0bbbd5453d4
Joseph Mutua <Mutuaj793@gmail.com>
2021-11-21 23:57:39 +0300

    Create the user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,29 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
-  console.log("REQ BODY ON SIGNUP", req.body);
+  //   console.log("REQ BODY ON SIGNUP", req.body);
+  const { name, email, password } = req.body;
+
+  User.findOne({ email })
+    .exec()
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          error: "Email is taken",
+        });
+      }
+      let newUser = new User({ name, email, password });
+
+      newUser.save();
+    })
+    .catch((err) => {
+      console.log("SIGNUP ERROR", err.message);
+      return res.status(400).json({
+        error: err,
+      });
+    });
 
   res.json({
-    data: "You hit the sigup endpoint",
+    message: "signup success! Please Sign in",
   });
 };
